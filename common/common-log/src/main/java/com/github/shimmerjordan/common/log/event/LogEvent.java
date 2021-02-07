69a671233f6c73435d0e1201ecdbdf7fe5a06efa
shimmerjordan <shimmerjordan@foxmail.com>
2021-02-07 23:59:12 +0800

    异步监听日志事件

--- common/common-log/src/main/java/com/github/shimmerjordan/common/log/event/LogEvent.java
+++ common/common-log/src/main/java/com/github/shimmerjordan/common/log/event/LogEvent.java
@@ -7,7 +7,7 @@ import org.springframework.context.ApplicationEvent;
  * 日志事件
  *
  * @author shimmerjordan
- * @date 2021/3/12 23:58
+ * @date 2021/03/12 23:58
  */
 public class LogEvent extends ApplicationEvent {
     public LogEvent(Log source) {
