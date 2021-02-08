b9a63e34bfcbb62b08b6dcebdcb5232175af4132
shimmerjordan <shimmerjordan@foxmail.com>
2021-02-08 18:23:58 +0800

    验证码常量配置

--- common/common-security/src/main/java/com/github/shimmerjordan/common/security/constant/SecurityConstant.java
+++ common/common-security/src/main/java/com/github/shimmerjordan/common/security/constant/SecurityConstant.java
@@ -2,7 +2,7 @@ package com.github.shimmerjordan.common.security.constant;
 
 /**
  * @author shimmerjordan
- * @date 2020/12-25 14:08
+ * @date 2020/02/25 14:08
  */
 public class SecurityConstant {
 
